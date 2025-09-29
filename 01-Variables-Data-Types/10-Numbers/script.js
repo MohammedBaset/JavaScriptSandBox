a58a058fab0b59e032989567e9096cd9281235e2
Mohammed Baset <basetemail@gmail.com>
2025-09-30 02:09:59 +1000

    Initial commit

--- 01-Variables-Data-Types/10-Numbers/script.js
+++ 01-Variables-Data-Types/10-Numbers/script.js
@@ -1,5 +1,50 @@
+//Creat number object using number constructor
+
 let x;
 
 const num = new Number(5);
 
-console.log(typeof num);
+// console.log(typeof num);
+
+x = num.toString();
+
+console.log(x);
+
+x = num.toString().length;
+
+console.log(x);
+
+x = num.toFixed(2)
+
+console.log(x);
+
+x = num.toPrecision(3);
+
+console.log(x);
+
+console.log(typeof x);
+
+x = num.toExponential(2)
+
+console.log(x);
+
+x = num.toLocaleString('en-US');
+
+console.log(x);
+
+// x = num.__proto__;
+
+console.log(num);
+
+x = num.valueOf();
+
+console.log(x);
+
+x = Number.MAX_VALUE;
+
+console.log(x);
+
+x = Number.MIN_VALUE;
+
+console.log(x);
+
