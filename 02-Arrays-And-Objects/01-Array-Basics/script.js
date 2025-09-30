d5b2797dedea60cd5af9b443c43d47e9b53096ed
Mohammed Baset <basetemail@gmail.com>
2025-10-01 04:32:01 +1000

    Initial commit

---
+++ 02-Arrays-And-Objects/01-Array-Basics/script.js
@@ -0,0 +1,56 @@
+let x;
+
+// Array Literal
+const numbers = [12, 45, 33, 29, 39, 102];
+
+x = numbers[0];
+
+console.log(x);
+
+x = numbers[0] + numbers[3];
+
+console.log(x);
+
+x = numbers.length
+
+console.log(x);
+
+//Mixed Array
+
+const mixed = [12, 'Hello', true, null];
+
+
+//Array Constructor
+
+const fruits = new Array('apple', 'grape', 'orange');
+
+x = `My favorite fruit is an ${fruits[2]}`;
+
+console.log(x);
+
+fruits[2] = 'pears';
+
+x = fruits;
+
+console.log(x);
+
+// fruits.length = 2;
+
+// x = fruits;
+
+// console.log(x);
+
+fruits[3] = 'strawberry';
+
+x = fruits;
+
+console.log(x);
+
+fruits[fruits.length]= 'blueberry';
+fruits[fruits.length]= 'peach';
+
+x = fruits;
+
+console.log(x);
+
+
